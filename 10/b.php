6065aa82084c1f0e77588fd1e79db3561f1c29de
Trupthi <sm.trupthi08@gmail.com>
2026-06-24 22:06:18 +0530

    part2

---
+++ 10/b.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post" action="">
+  Enter a number: <input type="text" name="num" />
+  <input type="submit" value="Check" />
+</form>
+<?php
+if (isset($_POST['num']) && $_POST['num'] !== "") {
+  $n = (int) $_POST['num'];
+  $isPrime = true;
+  if ($n < 2) {
+    $isPrime = false;
+  } else {
+    for ($i = 2; $i <= sqrt($n); $i++) {
+      if ($n % $i == 0) {
+        $isPrime = false;
+        break;
+      }
+    }
+  }
+  if ($isPrime) {
+    echo "<p>$n is a Prime number.</p>";
+  } else {
+    echo "<p>$n is not a Prime number.</p>";
+  }
+}
+?>
+</body>
+</html>
